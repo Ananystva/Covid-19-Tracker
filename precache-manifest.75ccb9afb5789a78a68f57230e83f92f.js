0075ba7f840d2691bbb341e50c77bc697f4760af
Ananystva <ananystva@gmail.com>
2021-06-13 05:17:32 +0530

    Updates

---
+++ precache-manifest.75ccb9afb5789a78a68f57230e83f92f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86466bc34bae745dfd4aa4ae8d8b9abb",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "bf254a9963cbdf36a790",
+    "url": "/Covid-19-Tracker/static/css/2.f72ce264.chunk.css"
+  },
+  {
+    "revision": "0128f4ca658c151183a5",
+    "url": "/Covid-19-Tracker/static/css/main.d4d97cab.chunk.css"
+  },
+  {
+    "revision": "bf254a9963cbdf36a790",
+    "url": "/Covid-19-Tracker/static/js/2.24100913.chunk.js"
+  },
+  {
+    "revision": "2eec0a263ef1f7393322e8c89c5ff82a",
+    "url": "/Covid-19-Tracker/static/js/2.24100913.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0128f4ca658c151183a5",
+    "url": "/Covid-19-Tracker/static/js/main.3e1d624d.chunk.js"
+  },
+  {
+    "revision": "4b644fa9ca0287604fad",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.b2c8fe1b.js"
+  }
+]);
